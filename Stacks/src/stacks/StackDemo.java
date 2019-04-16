4d56f8620d4cccb5d41918f183ea678a1692f065
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-04-16 07:50:17 -0400

    First Commit

---
+++ Stacks/src/stacks/StackDemo.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+package stacks;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		Stack myStack = new Stack();
+		myStack.push(42);
+
+	}
+
+}
